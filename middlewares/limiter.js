086692154da2f1891462607edf3ac5ca2d1a40be
Sasha Ivanova <aliaksandra.v.ivanova@gmail.com>
2020-11-15 12:11:05 +0100

    limiter

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000,
-  max: 100,
+  max: 1000,
   message: 'You have exceeded the 100 requests in 24 hrs limit!',
   headers: true,
 });
